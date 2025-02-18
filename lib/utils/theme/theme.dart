97c98910ab5b4c88d37a3e590d3edf7027b517ae
IntelligentBeaver <aman.211506@ncit.edu.np>
2025-02-18 15:45:45 +0545

    Folder Structure for the application, initial setup of fonts and icons, initial setup of themes.

---
+++ lib/utils/theme/theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:ordertracking_flutter/utils/theme/custom/text_theme.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  // *ThemeData created for both Light Theme and Dark Theme
+
+  /*
+  TODO: Add more Theme Classes
+  TODO: Add more attributes to theme data
+  */
+
+  static ThemeData lighttheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    textTheme: AppTextTheme.lightTextTheme,
+  );
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.dark,
+    fontFamily: 'Poppins',
+    primaryColor: Colors.blue,
+    textTheme: AppTextTheme.darkTextTheme,
+  );
+}
