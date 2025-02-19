d8819031a6fc9a776fdbd494fde175cf9f94e86a
IntelligentBeaver <aman.211506@ncit.edu.np>
2025-02-19 09:51:28 +0545

    Implementation of Theme Epic (OT -12, 13, 14, 15)

--- lib/app.dart
+++ lib/app.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:ordertracking_flutter/common/theme.provider.dart';
 import 'package:ordertracking_flutter/utils/theme/theme.dart';
+import 'package:provider/provider.dart';
 
 /*
-TODO: Theme switching integration (provider and sharedPreferences)
 TODO: Implement Custom Made Classes for fonts, icons and images in app
+TODO: Implement Localization using i18next
 */
 
 class App extends StatelessWidget {
@@ -12,10 +14,11 @@ class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
     return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: AppTheme.lighttheme,
+      theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
+      themeMode: themeProvider.themeMode,
       title: 'Order Tracker',
       home: Scaffold(
         appBar: AppBar(
@@ -25,8 +28,43 @@ class App extends StatelessWidget {
             style: TextStyle(fontWeight: FontWeight.w800),
           ),
         ),
-        body: const Center(
-          child: Text('Body.'),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 34),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text('Theme:'),
+
+                // * Dropdopwn Menu for changing the Theme of the app.
+                DropdownButton<ThemeMode>(
+                  value: themeProvider
+                      .themeMode, //The default value to be seen in the dropdopwn menu
+                  onChanged: (ThemeMode? newMode) {
+                    if (newMode != null) {
+                      themeProvider.setTheme(
+                          newMode); // Calls provider function to set the theme and store preference
+                    }
+                  },
+                  items: const [
+                    // The "value" is passed to the onChanged function as parameter
+                    DropdownMenuItem(
+                      value: ThemeMode.system,
+                      child: Text("System Theme"),
+                    ),
+                    DropdownMenuItem(
+                      value: ThemeMode.light,
+                      child: Text("Light Theme"),
+                    ),
+                    DropdownMenuItem(
+                      value: ThemeMode.dark,
+                      child: Text("Dark Theme"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:ordertracking_flutter/app.dart';
+import 'package:ordertracking_flutter/common/theme.provider.dart';
 import 'package:ordertracking_flutter/utils/theme/theme.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  ThemeProvider themeProvider = ThemeProvider();
+  await themeProvider.loadTheme();
+  runApp(
+    // *Using MutliProvider for further addition of providers
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => themeProvider,
+        ),
+      ],
+      child: const App(),
+    ),
+  );
 }
