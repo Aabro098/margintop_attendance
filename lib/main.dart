1df4fd286fb84c52040c521ae5213b1bdebb8704
IntelligentBeaver <aman.211506@ncit.edu.np>
2025-02-24 18:22:34 +0545

    Completed Notification System with Custom Button.

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:ordertracking_flutter/app.dart';
 import 'package:ordertracking_flutter/common/localization_provider.dart';
 import 'package:ordertracking_flutter/common/theme.provider.dart';
+import 'package:ordertracking_flutter/utils/helpers/notification_service.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   ThemeProvider themeProvider = ThemeProvider();
   LocalizationProvider languageProvider = LocalizationProvider();
+
+  // Notification Service initialization
+  NotificationService().initiNotification();
+
   await themeProvider.loadTheme();
   runApp(
     // *Using MutliProvider for further addition of providers
---
+++ lib/utils/helpers/notification_service.dart
@@ -0,0 +1,85 @@
+import 'dart:typed_data';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:flutter/material.dart';
+
+class NotificationService {
+// TODO: Add Support for maxOS and Linux Systems.
+// TODO: Create Various other functions for different type of notifications.
+  // _showNotificationWithActions()
+  // _showNotificationWithTextAction()
+  // _showNotificationWithIconAction()
+  // _showNotificationWithTextChoice()
+  // _showNotificationWithNoBody()
+  // _showNotificationWithNoTitle()
+  // _cancelNotification()
+  // _showNotificationCustomSound()
+  // _zonedScheduleNotification()
+  // _showNotificationWithNoSound()
+  // _showNotificationSilently()
+  // https://pub.dev/packages/flutter_local_notifications/example
+
+  //* Variables
+  final notificationsPlugin = FlutterLocalNotificationsPlugin();
+  bool _isInit = false;
+
+  //* Getters
+  bool get isInit {
+    return _isInit;
+  }
+
+  //! Initialization
+  Future<void> initiNotification() async {
+    if (_isInit) return; // already initialized
+
+    // ANDROID INITIALIZATION
+    const androidInitSettings =
+        AndroidInitializationSettings('@mipmap/ic_launcher');
+
+    // iOS INITIALIZATION
+    const iosInitSettings = DarwinInitializationSettings(
+      requestAlertPermission: true,
+      requestBadgePermission: true,
+      requestSoundPermission: true,
+    );
+
+    // LINUX INITIALIZATION
+    const linuxInitializationSettings =
+        LinuxInitializationSettings(defaultActionName: 'Open notification');
+
+    // COMMON INITIALIZATION
+    const initSettings = InitializationSettings(
+      android: androidInitSettings,
+      iOS: iosInitSettings,
+      macOS: iosInitSettings,
+      linux: linuxInitializationSettings,
+    );
+
+    // PLUGIN INITIALIZATION
+    await notificationsPlugin.initialize(initSettings);
+  }
+
+  //! Notification Detail Setup
+  NotificationDetails notificationDetails() => const NotificationDetails(
+      android: AndroidNotificationDetails(
+        'example1',
+        'example1 Notification',
+        channelDescription: 'Example 1 Channel',
+        importance: Importance.max,
+        priority: Priority.high,
+      ),
+      iOS: DarwinNotificationDetails());
+
+  //! Show Notification Section:
+  Future<void> showNotification({
+    int id = 0,
+    String? title,
+    String? body,
+  }) async {
+    return notificationsPlugin.show(
+      id,
+      title,
+      body,
+      notificationDetails(),
+    );
+  }
+}
